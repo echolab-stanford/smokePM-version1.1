7ea51d266ffc6fd6e46b6acc7a032d19830aae01
mmartin105 <marianamartins0415@gmail.com>
2025-05-15 14:32:58 -0700

    updated interpolation script used on revisions

--- main/16_02_interpolate_smokePM_to_grid.R
+++ main/16_02_interpolate_smokePM_to_grid.R
@@ -8,12 +8,12 @@ library(lubridate)
 source("scripts/setup/00_03_load_paths.R")
 
 # if the folder for saving the output doesn't exist, make it 
-if(!dir.exists(file.path(path_output_sherlock, "smokePM_interpolation", "smoke_grid_cell"))){
-  dir.create(file.path(path_output_sherlock, "smokePM_interpolation", "smoke_grid_cell"), recursive = T)
+if(!dir.exists(file.path(path_output_sherlock, "version1.1", "smokePM_interpolation", "revisions", "interpoaltions_full_model"))){
+  dir.create(file.path(path_output_sherlock, "version1.1", "smokePM_interpolation", "revisions", "interpoaltions_full_model"), recursive = T)
 }
 
 start_month = "2006-01" 
-end_month = "2023-12" 
+end_month = "2024-12" 
 
 # set crs for interpolation 
 interp_crs = "+proj=utm +zone=19 +datum=NAD83 +units=m +no_defs"
@@ -23,20 +23,18 @@ year_months = format(seq.Date(ym(start_month), ym(end_month), by = "month"), "%Y
 station_full_panel <- year_months %>% map(function(year_month) {
   y = substr(year_month, 1, 4)
   m = substr(year_month, 6, 7)
-  out = readRDS(file.path(path_data_sherlock, "3_intermediate", "station_smokePM_MM", sprintf("station_smokePM_%s_%s.rds", y, m))) %>% 
+  out = readRDS(file.path(path_data_sherlock, "3_intermediate", "station_smokePM_auto", sprintf("station_smokePM_%s_%s.rds", y, m))) %>% 
     dplyr::filter(!is.na(pm25) &  !is.na(smokePM)) # interpolate even for the smoke_missing dates, just use the filled smoke PM2.5
   return(out)
 }) %>% list_rbind()
 station_full_panel %<>% ungroup
-# station_full_panel <- readRDS("~/BurkeLab Dropbox/projects/smokePM-prediction/data/3_intermediate/station_smokePM.rds") %>% ungroup
-# station_full_panel %<>% mutate(id_orig = id, id = formatC(id, width = 9, flag =  "0"))
-# station_full_panel %<>% filter(year == 2006)
+
 
 # load 10km grid cell cetroids
 grid_cent <- readRDS(file.path(path_data_sherlock, "1_grids", "10km_cell_centroids.rds"))
 
 # load epa locations
-epa_ll <- st_read(file.path(path_data_sherlock, "EPA", "epa_station_locations"))
+epa_ll <- st_read(file.path(path_data_sherlock, "EPA", "epa_station_locations_auto"))
 
 # how the folds are constructed from 16_02 
 grid_merra = raster(file.path(path_data_sherlock, "1_grids", "merra_folds.nc"))
@@ -64,8 +62,8 @@ station_full_panel %<>% dplyr::select(id, fold, station_x, station_y,
                                       year, month, date, smoke_day, smokePM)
 
 # load best interpolation params 
-interp_parameters <- readRDS(file.path(path_output_sherlock, "smokePM_interpolation",
-                                       "interp_tune_params_drop9999.rds")) %>%
+interp_parameters <- readRDS(file.path(path_output_sherlock, "version1.1", "smokePM_interpolation", "revisions",
+                                       "interp_tune_params_drop9999_200601_202412.rds")) %>%
   magrittr::extract2("Best_Par") %>%
   as.list
 
@@ -106,11 +104,15 @@ map_chr(year_months, function(y_m){
   print(y_m)
   y_i = substr(y_m, 1, 4) %>% as.numeric
   m_i = substr(y_m, 6, 7) %>% as.numeric
-  out_file <- file.path(path_output_sherlock, "smokePM_interpolation", "smoke_grid_cell", 
+  
+  out_file <- file.path(path_output_sherlock, "version1.1", "smokePM_interpolation", "revisions", 
                         paste0("smokePM_interpolated_", gsub("-", "_", y_m), ".rds"))
+  
   smoke_day_cells <- readRDS(file.path(path_data_sherlock, "3_intermediate", "filled_smoke", paste0("filled_smoke_days_", gsub("-", "_", y_m), ".rds"))) %>% 
     left_join(grid_cent %>% dplyr::select(grid_id_10km, grid_x = COORDX, grid_y = COORDY), by = "grid_id_10km")
+  
   print(paste0(nrow(smoke_day_cells), " smoke day-grid cells"))
+  
   interp_over_dates(station_full_panel %>% filter(year == y_i & month == m_i), # data frame with x, y, date_col, and value_col
                     smoke_day_cells %>% rename(id = grid_id_10km), 
                     list(idp = interp_parameters$pow,
